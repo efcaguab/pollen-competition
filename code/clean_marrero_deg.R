27c6cc053cb843bc056091164a6ef3ecd9f26715
Fernando Cagua <efcaguab@gmail.com>
2017-11-16 17:47:53 +1300

    Clean degrees

--- code/clean_marrero_deg.R
+++ code/clean_marrero_deg.R
@@ -1,17 +1,31 @@
 # Compile degree data from visitation networks in Hugo's work
 
-library(dplyr)
+lapply(c("dplyr", "readr", "tidyr", "stringr"), 
+       function(x) library(x, character.only = T))
 
 # script arguments 
 args <- commandArgs(trailingOnly = TRUE)
-input_file <- args[1]
-output_file <- args[2]
+vis_file <- args[1]
+qual_vis_file <- args[2]
+output_file <- args[3]
 
-# # testing defaults
-# input_file <- "./data/datasets/processed/visitation/marrero.rds"
-# output_file <- "./data/datasets/processed/degree/marrero.rds"
+# testing defaults
+vis_file <- "../data/datasets/processed/visitation/marrero.rds"
+qual_vis_file <- "../data/datasets/raw/marrero2016/qualitative_visits.csv" 
+output_file <- "../data/datasets/processed/degree/marrero.rds"
 
-vis <- readRDS(input_file)
+vis <- readRDS(vis_file)
+
+qua <- read_csv(qual_vis_file)
+qua %>% 
+  mutate(plant_name = plant, 
+         plant_name = tolower(plant_name),
+         # assume that cf is close to the actual species. Currently only
+         # ocurring for *Noticastrum acuminatum*
+         plant_name = str_replace(plant_name, " sp. cf. ", " "), 
+         plant = plant_name) %>%
+  arrange(plant_name) %>%
+  separate(plant, c("genus", "species"), sep = " ") %>% View
 
 deg <- plyr::ldply(vis, function(x) x$visitation) %>%
   group_by(plant_name) %>%
