ca345de5bcbf92c7593d3e653c9c25c4fb09f831
Fernando Cagua <efcaguab@gmail.com>
2018-02-20 09:56:26 +1300

    Add some analysis and reporting

--- main.R
+++ main.R
@@ -1,9 +1,16 @@
+# basic R
+library(stats)
+library(utils)
+# additional packages
 library(dplyr)
 library(foreach)
 library(drake)
 library(tools)
 library(stringr)
 library(magrittr)
+library(knitr)
+library(rmarkdown)
+library(xtable)
 
 # load functions
 functions_folder <- './functions'
@@ -22,14 +29,30 @@ clean_data <- drake_plan(
 )
 
 format_data <- drake_plan(
-  pollination_gain = con_hetero_specific(armonised_data, sites)
+  dep_frame = extract_dep_frame(armonised_data),
+  # control_dep_plant = extract_closed_treatment_means(dep_frame, c("season", "site_name", "plant")),
+  # control_dep_site = extract_closed_treatment_means(dep_frame, c("season", "site_name")),
+  # control_dep_global = extract_closed_treatment_means(dep_frame, NULL), 
+  strings_in_dots = 'literals'
+)
+
+analysing <- drake_plan(
+  consp_self = model_conspecific_self(dep_frame),
+  significant_gain_global = mann_withney_part_df(dep_frame, by = 'recipient', var = 'treatment'),
+  significant_gain_site = mann_withney_part_df(dep_frame, by = c('recipient', 'site_name'), var = 'treatment'),
+  strings_in_dots = 'literals'
+)
+
+reporting <- drake_plan(
+  'publication/supp_info.pdf' = render('publication/supp_info.Rmd', quiet = TRUE),
+  file_targets = TRUE
 )
 
 # set up plan
-project_plan <- rbind(clean_data, format_data)
+project_plan <- rbind(clean_data, format_data, analysing, reporting)
 project_config <- drake_config(project_plan)
 vis_drake_graph_sml0(project_config)
 
 # execute plan
-make(clean_data)
+make(project_plan)
 vis_drake_graph_sml0(project_config)
