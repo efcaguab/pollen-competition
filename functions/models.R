e7cc3d8425ecd23af0545121c2cfa21723eab89e
Fernando Cagua <efcaguab@gmail.com>
2018-02-20 14:00:32 +1300

    Calculate confidence intervals in wilcox tests

--- functions/models.R
+++ functions/models.R
@@ -15,14 +15,14 @@ mann_withney_part_df <- function(x, by = 'recipient', categ = 'conspecific', ...
 }
 
 # test mann withney inside a data frame column
-mann_withney_df <- function(x, var, treatments = c('open', 'closed'), alternative = 'greater'){
+mann_withney_df <- function(x, var, treatments = c('open', 'closed'), alternative = 'greater', conf.int = FALSE){
   a <- x %>% 
     filter_at(var, any_vars(. == treatments[1])) %$% 
     pollen_density
   b <- x %>% 
     filter_at(var, any_vars(. == treatments[2])) %$%
     pollen_density
-  wilcox.test(a, b, alternative)
+  wilcox.test(a, b, alternative, conf.int = conf.int)
 }
 
 
