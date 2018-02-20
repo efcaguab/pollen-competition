e9b5a3055ac680d4ba01fb26d02e312619b6f325
Fernando Cagua <efcaguab@gmail.com>
2018-02-20 14:00:46 +1300

    Functions to summarise test results

---
+++ functions/summarise_info.R
@@ -0,0 +1,53 @@
+
+#' Produce a table with summary statistics
+#'
+#' @param y a list of models
+#' @param fun function to tabulate with a data frame as return. Defaults to 
+#'
+#' @return a data frame 
+#' 
+glance_table <- function(y, fun = broom::tidy){
+  y %>% plyr::ldply(function(x){
+    if(class(x) != "try-error"){
+      fun(x)
+    } else {
+      NULL
+    }
+  })
+}
+
+#' Shorten species names 
+#'
+#' From Genus species to G. species. Ignores Genus sp.
+#'
+#' @param x a character vector
+#'
+#' @return a character vector
+#'
+shorten_sp_name <- function(x){
+  y <- x
+  for(i in 1:length(x)){
+    if(!grepl('sp\\.', x[i])) {
+      y[i] <- mini_dot(x[i])
+    } 
+  }
+  y
+}
+
+#' Shorten species names
+#'
+#' #' From Genus species to G. species. Ignores Genus sp.
+#' 
+#' @param x a character string
+#'
+#' @return a character string
+#'
+mini_dot <- function(x){
+  space_location <- stringr::str_locate(x, ' ')
+  if(!any(is.na(space_location))){
+    stringr::str_sub(x, 2, space_location[1]) <- ". "
+    x
+  } else{
+    x
+  }
+}
