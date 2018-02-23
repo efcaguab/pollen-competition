225a77aa0c507892f348b06a43c48096712baf67
Fernando Cagua <efcaguab@gmail.com>
2018-02-24 11:34:21 +1300

    Add basic documentation

--- functions/bootstraping.R
+++ functions/bootstraping.R
@@ -6,6 +6,7 @@
 #'
 #' @return a data frame with pollen_gain column
 #'
+data_replicate <- function(de, category, ab, ph, transformation = I, dummy_id){
   
   # de <- readd(dep_frame)
   # ab <- readd(plant_rel_abu)
@@ -23,17 +24,34 @@
     left_join(per_community(ab), by = c('plant_name', 'site_name'))
 }
 
+#' Get global values per plant
+#'
+#' @param x data frame with some colums that contain the string 'tot'
+#'
+#' @return a data frame organised per plant
+#' 
 per_plant <- function(x){
   x %>%
     group_by(plant_name) %>%
     summarise_at(vars(contains('tot')), unique)
 }
 
+#' Select only community relevant columns in a data frame
+#'
+#' @param x a data frame with plant and site name
+#'
 per_community <- function(x){
   x %>% 
     select_at(vars(-contains('tot')))
 }
 
+#' Sample the deposition data to get a boostrap data replicate 
+#'
+#' @param x dep_frame
+#' @param category either 'con' or 'het'
+#'
+#' @return a dataframe with bootstrap replicates
+#' 
 get_deposition_sampled_data <- function(x, category){
   
   x %>%
