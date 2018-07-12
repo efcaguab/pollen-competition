d571a988bd70980a993477e5a4d801a5c8d3361c
Fernando Cagua <efcaguab@gmail.com>
2018-07-12 15:25:55 -0400

    Use contribution to overlap for pollen transfer metric

--- code/pollen_overlap.R
+++ code/pollen_overlap.R
@@ -7,9 +7,19 @@ get_pollen_overlap <- function(tra_frame){
     dplyr::summarise(grain = sum(grain)) %>%
     tidyr::spread(individual, grain, fill= 0) 
   
-  global_and_site_overlap(grain_matrix, function(x) log(x + 1), horn_fun) %>%
+  # global_and_site_overlap(grain_matrix, function(x) log(x + 1), horn_fun) %>%
+  #   dplyr::mutate(var_trans = 'log') %>%
+  #   dplyr::group_by(scale) %>%
+  #   dplyr::mutate(pov = scale(tov)) %>%
+  #   dplyr::rename(pollen_overlap = tov)
+  # 
+  global_and_site_overlap(grain_matrix, function(x) log(x + 1), overlap_contrib) %>%
     dplyr::mutate(var_trans = 'log') %>%
     dplyr::group_by(scale) %>%
     dplyr::mutate(pov = scale(tov)) %>%
     dplyr::rename(pollen_overlap = tov)
+  
+  # red <- . %>% dplyr::filter(scale == "community")
+  # 
+  # plot(red(a)$pov, red(b)$pov)
 }
