5ec80c6de71fae3862cd0e69f1f4b7aca9e00070
Fernando Cagua <efcaguab@gmail.com>
2018-09-03 21:29:10 +0200

    Some aesthetic changes to figure of quantitative qualitative

--- code/make_fig_con_hetero_gain.R
+++ code/make_fig_con_hetero_gain.R
@@ -26,40 +26,46 @@ make_fig_con_hetero_gain <- function(tidied_fixed, model_linear_fits, model_form
     dplyr::sample_n(size = 1, weight = weight) %>%
     dplyr::group_by()
   
+  exp_minus_one <- function(x){
+    exp(x)-1
+  }
+  
   points_sp_summarised <- points_sp %>%
     dplyr::group_by(plant_name, site_name) %>%
     dplyr::summarise_at(c("conspecific", "heterospecific"), 
-                        dplyr::funs(median, quantile_05,quantile_95))
+                        dplyr::funs(median, quantile_05,quantile_95)) %>%
+    dplyr::mutate_if(is.numeric, I) 
 
   dplyr::data_frame(x = get_pred_range(tidied_fixed, "heterospecific"),
                    y = get_pred_range(tidied_fixed, "conspecific")) %>%
     ggplot() +
-    geom_abline(data = ab_lines, 
-                aes(slope = slope, intercept = elevation), 
-                size = 0.1, linetype = 1, colour = "grey", 
-                alpha = 0.5) +
-    
-    geom_errorbar(data = points_sp_summarised, 
-                  aes(x = heterospecific_median, ymin = conspecific_quantile_05, ymax = conspecific_quantile_95), 
-                  show.legend = F, alpha = 0.5, colour = "grey") + 
-    geom_errorbarh(data = points_sp_summarised, 
-                  aes(x = heterospecific_median, 
+    geom_abline(data = ab_lines,
+    aes(slope = slope, intercept = elevation),
+    size = 0.1, linetype = 1, colour = "grey",
+    alpha = 0.5) +
+    geom_errorbar(data = points_sp_summarised,
+                  aes(x = heterospecific_median, ymin = conspecific_quantile_05, ymax = conspecific_quantile_95),
+                  show.legend = F, alpha = 0.5, colour = "grey") +
+    geom_errorbarh(data = points_sp_summarised,
+                  aes(x = heterospecific_median,
                       y = conspecific_median,
-                      xmin = heterospecific_quantile_05, xmax = heterospecific_quantile_95), 
-                  show.legend = F, alpha = 0.5, colour = "grey") + 
+                      xmin = heterospecific_quantile_05, xmax = heterospecific_quantile_95),
+                  show.legend = F, alpha = 0.5, colour = "grey") +
     geom_point(data = points_sp_summarised,
-               aes(x = heterospecific_median, y = conspecific_median), 
-               alpha = 1, show.legend = FALSE, shape = 21, colour = "black", size = 1) +
+    aes(x = heterospecific_median, y = conspecific_median),
+    alpha = 1, show.legend = T, shape = 21, colour = "black", size = 1) +
     geom_abline(data = dplyr::summarise_if(ab_lines, is.numeric, median),
                 aes(slope = slope, intercept = elevation),
                 size = 0.5, linetype = 1, colour = "black") +
     geom_abline(slope = 1, intercept = 0, size = 0.25, linetype = 2) +
     geom_hline(yintercept = 0, size = 0.25, linetype = 2) +
-    coord_equal(xlim = get_pred_range(tidied_fixed, "heterospecific"),
-                ylim = get_pred_range(tidied_fixed, "conspecific")) +
+    coord_cartesian(xlim = c(0, get_pred_range(tidied_fixed, "heterospecific")[2]),
+                    ylim = c(0, get_pred_range(tidied_fixed, "conspecific"))) +
+    # scale_x_continuous(breaks = seq(0,10, 2)) +
+    # scale_y_continuous(breaks = seq(0,10,2)) +
     pub_theme() +
     scale_color_discrete() +
-    labs(x = "gain in heterospecific pollen", 
+    labs(x = "gain in heterospecific pollen",
          y = "gain in conspecific pollen")
 }
 
