225f6ea0df95a7e0f0459b042516681805316473
Fernando Cagua <efcaguab@gmail.com>
2018-09-19 11:37:38 +0200

    Adjust figure of log transformation for relative and absolute pollen amo

--- code/fig_pollen_density_diff.R
+++ code/fig_pollen_density_diff.R
@@ -9,12 +9,13 @@ plot_dist <- function(fun, rep_1){
   rep_1 %>%
     dplyr::filter(scale == 'imputed', 
                   var_trans == 'log') %>%
-    dplyr::mutate_at(c('open', 'closed'), fun) %>% 
-    ggplot(aes(x = open - closed, colour = pollen_category)) +
+    dplyr::mutate_at(c('open', 'closed', 'pollen_gain'), fun) %>% 
+    humanize() %>% 
+    ggplot(aes(x = pollen_gain, colour = pollen_category)) +
     stat_density(position = position_identity(), geom = "line") +
     pub_theme() +
-    scale_color_manual(values = c_scale()) +
+    scale_color_manual(values = rev(RColorBrewer::brewer.pal(4, "OrRd"))) +
     theme(legend.position = "top") + 
-    labs(x = "gain in pollen density (open-closed)", 
+    labs(x = "pollen grains per stigma",
          colour = "pollen type")
 }
