597510325412f4d80c39c7f5344a7022d2be8cd5
Fernando Cagua <efcaguab@gmail.com>
2018-10-12 16:07:56 +1300

    Use different cache for abc figures plan

--- presentations/abc2018/make_figures.R
+++ presentations/abc2018/make_figures.R
@@ -12,25 +12,26 @@ library(magrittr)
 f1 <- lapply(list.files("code", full.names = T), source)
 f2 <- lapply(list.files("presentations/abc2018/figures_code", full.names = T), source)
 
+abc_cache <- drake::recover_cache(".drake_abc")
+
 # Plan --------------------------------------------------------------------
 
-getwd()
+drake::default_cache_path()
+
 # load needed data
-load_data_plan <- drake::drake_plan(
-  abc_tidied_fixed = drake::readd("tidied_fixed", character_only = T), 
-  abc_model_linear_fits = drake::readd("model_linear_fits", character_only = T), 
-  abc_model_formula_ranking = drake::readd("model_formula_ranking", character_only = T), 
-  abc_model_linear_fits_species = drake::readd("model_linear_fits_species", character_only = T)
-)
+tidied_fixed = drake::readd("tidied_fixed", character_only = T)
+model_linear_fits = drake::readd("model_linear_fits", character_only = T)
+model_formula_ranking = drake::readd("model_formula_ranking", character_only = T)
+model_linear_fits_species = drake::readd("model_linear_fits_species", character_only = T)
 
 figure_het_con_plan <- drake::drake_plan(
-  abc_fig_con_hetero_elements = get_figure_elements(abc_tidied_fixed, abc_model_linear_fits, abc_model_formula_ranking, abc_model_linear_fits_species)
+  fig_con_hetero_elements = get_figure_elements(tidied_fixed, model_linear_fits, model_formula_ranking, model_linear_fits_species),
+  fig_canvas = make_fig_het_con_abc(fig_con_hetero_elements, tidied_fixed, rep("white", 2))
 )
 
-abc_figures_plan <- rbind(
-  load_data_plan, 
+figures_plan <- rbind(
   figure_het_con_plan
 )
 
-abc_config <- drake::drake_config(abc_figures_plan)
-drake::make(abc_figures_plan)
+abc_config <- drake::drake_config(figures_plan, cache = abc_cache)
+drake::make(figures_plan, cache = abc_cache)
