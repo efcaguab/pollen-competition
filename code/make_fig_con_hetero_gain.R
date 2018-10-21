6d148d69e36ec0cd71d8443c0443f10c830b06d7
Fernando Cagua <efcaguab@gmail.com>
2018-10-22 11:31:27 +1300

    Use the points and linear models intead of SMA

--- code/make_fig_con_hetero_gain.R
+++ code/make_fig_con_hetero_gain.R
@@ -8,19 +8,6 @@ make_fig_con_hetero_gain <- function(tidied_fixed, model_linear_fits, model_form
                   weight = get_weights(likelyhood)) %>%
     dplyr::select(fixed_formula, weight)
   
-  ab_lines <- model_linear_fits %>%
-    dplyr::filter(var_trans == "log", 
-                  scale == "community") %>%
-    tidyr::spread(data = ., key = sma_parameter, value = value) %>%
-    dplyr::inner_join(model_weights, by = "fixed_formula") %>%
-    dplyr::group_by(model, scale) %>%
-    dplyr::sample_n(size = 1, weight = weight) %>%
-    dplyr::group_by()
-  
-  ab_lines_summarised <- ab_lines %>%
-    dplyr::group_by(rel) %>%
-    dplyr::summarise_if(is.numeric, median)
-  
   points_sp <- model_linear_fits_species %>%
     dplyr::filter(var_trans == "log",
                   scale == "community") %>% 
@@ -51,52 +38,63 @@ make_fig_con_hetero_gain <- function(tidied_fixed, model_linear_fits, model_form
   minor_breaks <- log(minor_labs + 1)
 
   p <- dplyr::data_frame(x = get_pred_range(tidied_fixed, "heterospecific"),
-                   y = get_pred_range(tidied_fixed, "conspecific")) %>%
+                         y = get_pred_range(tidied_fixed, "conspecific")) %>%
     ggplot() +
     geom_abline(slope = 1, intercept = 0, size = 0.25, linetype = 2) +
     geom_errorbar(data = points_sp_summarised,
-                  aes(x = heterospecific_median, 
-                      ymin = conspecific_quantile_05, 
-                      ymax = conspecific_quantile_95, 
+                  aes(x = heterospecific_median,
+                      ymin = conspecific_quantile_05,
+                      ymax = conspecific_quantile_95,
                       colour = rel),
                   show.legend = F, alpha = 0.25) +
     geom_errorbarh(data = points_sp_summarised,
-                  aes(x = heterospecific_median,
-                      y = conspecific_median,
-                      xmin = heterospecific_quantile_05, 
-                      xmax = heterospecific_quantile_95, 
-                      colour = rel),
-                  show.legend = F, alpha = 0.25) +
+                   aes(x = heterospecific_median,
+                       y = conspecific_median,
+                       xmin = heterospecific_quantile_05,
+                       xmax = heterospecific_quantile_95,
+                       colour = rel),
+                   show.legend = F, alpha = 0.25) +
     geom_point(data = points_sp_summarised,
-               aes(x = heterospecific_median, 
-                   y = conspecific_median, 
+               aes(x = heterospecific_median,
+                   y = conspecific_median,
                    colour = rel),
                alpha = 1, show.legend = T, shape = 21, size = 1) +
-    geom_abline(data = ab_lines,
-                aes(slope = slope, 
-                    intercept = elevation, 
-                    colour = rel),
-                size = 0.1, linetype = 1,
-                alpha = 0.25) +
-    geom_abline(data = ab_lines_summarised, 
-                aes(slope = slope, intercept = elevation, colour = rel), 
-                size = 0.5, linetype = 1) + 
-    # geom_hline(yintercept = 0, size = 0.25, linetype = 2) +
+    geom_smooth(data = points_sp,
+                aes(x = heterospecific_abs,
+                    y = conspecific,
+                    colour = con_type, 
+                    group = interaction(con_type, model)),
+                size = 0.1, 
+                linetype = 1,
+                alpha = 0.25,
+                show.legend = T,
+                method = "lm", se = F) +
+    geom_smooth(data = points_sp,
+                aes(x = heterospecific_abs,
+                    y = conspecific,
+                    colour = con_type),
+                size = 0.5, 
+                linetype = 1,
+                alpha = 1,
+                show.legend = T,
+                method = "lm", se = F) +
     scale_x_continuous(breaks = major_breaks, labels = major_labs, minor_breaks = minor_breaks) +
     scale_y_continuous(breaks = major_breaks, labels = major_labs, minor_breaks = minor_breaks) +
-    scale_colour_manual(values = rev(RColorBrewer::brewer.pal(4, "OrRd"))) +
+    scale_colour_manual(values = rev(RColorBrewer::brewer.pal(4, "OrRd")),
+                        labels = c("relative", "absolute", "control")) +
     pub_theme() +
-    labs(x = "heterospecific pollen density gain",
-         y = "conspecific pollen density gain") +
-    theme(legend.position = c(0.01,0.98), 
-          legend.direction = "horizontal", 
-          legend.justification = c(0,1), 
-          legend.title = element_blank(), 
-          legend.background = element_rect(fill = "white"), 
-          panel.grid.major = element_line(size = 0.25), 
+    labs(x = "heterospecific pollen density",
+         y = "conspecific pollen density") +
+    theme(legend.position = c(0.01,0.98),
+          legend.direction = "horizontal",
+          legend.justification = c(0,1),
+          legend.title = element_blank(),
+          legend.background = element_rect(fill = "white"),
+          panel.grid.major = element_line(size = 0.25),
           axis.title = element_text(size = 8, colour = "grey20"))
   p
-  # pdf(width = 3.25, height = 3.25)
+
+  # pdf(width = 3.25, height = 4.25)
   # p
   # dev.off()
 }
