5a0ae32441b113ac25e3ce5e9326d9532cc9951a
Fernando Cagua <efcaguab@gmail.com>
2018-10-24 17:29:57 +1300

    setting up ggplot theme for presentation figures

---
+++ presentations/abc2018/figures_code/plotting_theme.R
@@ -0,0 +1,17 @@
+# theme for abc presentation figures
+abc_theme <- function(){
+    theme(text = element_text(size = 16, colour = "white", family = "Space Mono"), 
+          axis.text = element_text(colour = "grey75"),
+          plot.title = element_text(face = "bold"),
+          rect = element_rect(fill = "black"),
+          line = element_line(colour = "grey10"),
+          panel.background = element_rect(fill = "black", colour = "grey75"),
+          plot.background = element_rect(colour = "black"),
+          panel.grid.major = element_line(colour = "grey30"),
+          panel.grid.minor = element_line(colour = "grey20"),
+          strip.background = element_blank(),
+          strip.text = element_text(hjust = 0),
+          legend.title = element_text(hjust = 0.5), 
+          legend.key.size = unit(3, "mm"), 
+          legend.margin = margin())
+}
