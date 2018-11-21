5316769905a4183dfbe212c0880bc1a8dd198ec3
Fernando Cagua <efcaguab@gmail.com>
2018-11-21 18:21:22 +1300

    plot variable importance

--- code/get_variable_importance.R
+++ code/get_variable_importance.R
@@ -31,3 +31,68 @@ get_variable_importance <- function(model_formula_ranking){
     tidyr::spread(pollen_category, importance) %>%
     dplyr::arrange(dplyr::desc(heterospecific))
 }
+
+plot_variable_importance <- function(variable_importance) {
+  require(ggplot2)
+  
+  plot_importance <- function(x){
+    middle_space <- -0.35
+    x %>% ggplot(aes(x = term, y = value)) +
+      # geom_segment(aes(xend = term, yend = 0)) +
+      # geom_point(shape = 21, fill = cgm()$pal_rb3[2], size = 6) +
+      
+      geom_col(fill = "grey70") +
+      geom_hline(yintercept = 0, linetype = 1, size = 0.25, colour = "grey30") +
+      
+      geom_point(aes(x = 1, y = middle_space), alpha = 0) +
+      geom_text(aes(label = paste0(" ", format(round(value, digits = 2)), " "), 
+                    hjust = text_align), 
+                size = 2) +
+      geom_text(aes(label = term, y = middle_space), 
+                # fontface = "bold", 
+                size = 2.5, 
+                colour = "black") +
+      coord_flip() +
+      pub_theme() +
+      theme(axis.title.y = element_blank(), 
+            legend.position = "none", 
+            panel.border = element_blank(),
+            axis.ticks = element_blank(),
+            axis.line.y = element_line(color = "red"), 
+            axis.title.x = element_blank(), 
+            axis.text = element_blank())
+  }
+  
+  plots <- variable_importance %>%
+    tidyr::gather("key", "value", `conspecific (absolute)`:heterospecific) %>%
+    dplyr::filter(key %in% c("conspecific (absolute)", "heterospecific")) %>%
+    dplyr::mutate(term = forcats::fct_reorder(term, value), 
+                  text_align = dplyr::if_else(key == "heterospecific", 
+                                              "right", "left")) %>%
+    split(.$key) %>%
+    purrr::map(plot_importance)
+  
+  plots <- plots %>%
+    # subtitles
+    purrr::map2(c("conspecific pollen", "heterospecific pollen"), 
+              function(x,y) {x + labs(subtitle = y)}) %>%
+    purrr::map2(list(margin(r = 0, t = 5.5, b = 5.5, l = 5.5), margin(l = 0, r = 5.5, t = 5.5, b = 5.5)), 
+                function(x,y) {x + theme(plot.margin = y)})
+  
+  plots[[1]] <- plots[[1]] + labs(title = "(a) relative variable importance")
+  plots[[2]] <- plots[[2]] + scale_x_discrete(position = "top")
+  plots[[1]] <- plots[[1]] + scale_y_reverse(expand = c(0,0))
+  plots[[2]] <- plots[[2]] + scale_y_continuous(expand = c(0,0))
+  plots[[2]] <- plots[[2]] + theme(plot.subtitle = element_text(hjust = 1))
+  plots[[1]] <- plots[[1]] + theme(plot.title = element_text(margin = margin(b = 5.5, unit = "pt")))
+  
+  # pdf(width = 3.25 , height = 1.25)
+  cowplot::plot_grid(
+    plotlist = plots, 
+    ncol = 2, 
+    align = "h"
+  )
+  # dev.off()
+}
+
+
