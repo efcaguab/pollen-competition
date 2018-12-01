13c13ce57d5b127f86bb6b1078e05a9dceb73681
Fernando Cagua <efcaguab@gmail.com>
2018-12-01 21:04:57 +1300

    Add custom labels instead of text in y axis coef avg

--- code/fig_coefficient_averages.R
+++ code/fig_coefficient_averages.R
@@ -58,20 +58,61 @@ make_fig_coefficient_avarages <- function(coefficient_averages, variable_importa
   
   dist %<>% 
     dplyr::inner_join(imp) %>%
-    dplyr::group_by() 
-  
-  p <- dist %>%
+    dplyr::group_by() %>%
     dplyr::mutate(pollen_category = dplyr::if_else(pollen_category == "conspecific (absolute)", 
                                                    "conspecific", 
-                                                   pollen_category)) %>%
-    dplyr::mutate(term = forcats::fct_relevel(term, c("# shared pol.", "func. originality", "abundance"), after = 0)) %>%
+                                                   pollen_category))
+  
+  labels <- dist %>%
     dplyr::filter(estimate != 0) %>%
-    ggplot(aes(x = estimate,
-               y = term
-               # colour = pollen_category, 
-               # vline_color = pollen_category, 
-               # fill = pollen_category
-               )) +
+    dplyr::mutate(x = estimate, categ1 = term, categ2 = pollen_category) %>%
+    dplyr::mutate(min_min = min(x), 
+                  max_max = max(x)) %>%
+    dplyr::group_by(categ1) %>%
+    dplyr::mutate(min_x = min(x), 
+                  max_x = max(x),
+                  inner = min(abs(min_x), abs(max_x)),
+                  outer = max(abs(min_x), abs(max_x)), 
+                  mean_x = mean(unique(c(min_x, max_x)))) %>% 
+    dplyr::select(categ1, categ2, min_min, max_max, min_x, max_x, inner, outer, mean_x) %>%
+    dplyr::distinct() %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      sign_outer = dplyr::if_else(
+        which.max(c(abs(min_x), abs(max_x))) == 1,
+        sign(min_x), sign(max_x)),
+      sign_inner = dplyr::if_else(
+        which.min(c(abs(min_x), abs(max_x))) == 1,
+        sign(min_x), sign(max_x)),
+    x_coord = inner * sign_inner,
+    x_coord_labs = mean_x, 
+    x_coord_title = mean_x,
+    x_align = dplyr::if_else(x_coord == max_x, 
+                             "left", "right"),
+    x_align_labs = dplyr::if_else(x_coord == min_x, "left", "right"),
+    sign_title = dplyr::if_else(x_align == "right", 
+                                   -1, 1),
+    x_coord = dplyr::if_else(categ2 == "func. originality", 
+                             outer * sign_outer, x_coord)) %>%
+    dplyr::rename(term = categ1, pollen_category = categ2) %>%
+    dplyr::group_by() %>%
+    dplyr::mutate(term_n = dplyr::case_when(
+      term == "# shared pol." ~ 4, 
+      term == "func. originality" ~ 3, 
+      term == "abundance" ~ 2, 
+      TRUE ~ 1
+    ))
+  p <- dist  %>%  
+    dplyr::filter(estimate != 0) %>%
+    dplyr::mutate(term_n = dplyr::case_when(
+      term == "# shared pol." ~ 4, 
+      term == "func. originality" ~ 3, 
+      term == "abundance" ~ 2, 
+      TRUE ~ 1
+    ), 
+                  pollen_category = forcats::fct_relevel(pollen_category, c("conspecific", "heterospecific")), 
+                  pollen_category = forcats::fct_rev(pollen_category)) %>%
+    ggplot() +
     geom_vline(xintercept = 0, linetype = 2, size = 0.25, color = "grey30") +
     geom_density_ridges(alpha = 0.8, 
                         panel_scaling = T, 
@@ -82,24 +123,24 @@ make_fig_coefficient_avarages <- function(coefficient_averages, variable_importa
                         vline_linetype = 1, 
                         size = 0.25, 
                         rel_min_height = 0, 
-                        bandwidth = 0.015, 
-                        colour = "grey30") +
-    # geom_text(data = imp, 
-    #           aes(label = label, y = as.numeric(term),
-    #               x = x, size = importance), 
-    #           fill = "white", 
-    #           # size = 2.65, 
-    #           nudge_y = 0.2, 
-    #           nudge_x = -0.04,
-    #           fontface = "bold") +
-    # geom_text(data = min_values, 
-    #           aes(label = title, x = x, y = 4.9), 
-    #           fontface = "bold",
-    #           size = 2.2, 
-    #           nudge_x = 0.06, 
-    #           hjust = "right", 
-    #           lineheight = 0.7) +
-    # coord_flip() +
+                        bandwidth = 0.015) +
+    geom_text(data = labels, 
+              aes(x = x_coord + sign_title * 0.075, 
+                  colour = pollen_category,
+                  y = pollen_category,
+                  label = abb_col(pollen_category), 
+                  hjust = x_align), 
+              nudge_x = 0.01,
+            stat = "unique", show.legend = F, 
+            size = 2) +
+    geom_text(data = labels %>% dplyr::select(-pollen_category) %>% dplyr::distinct(), 
+              aes(x = x_coord_title,
+                  y = 0.25, 
+                  label = term, 
+                  hjust = "center"), 
+              fontface = "plain",
+              stat = "unique", show.legend = F, 
+              size = 2.5) +
   # geom_hline(yintercept = 0.5, colour = "red") +
     facet_grid(pollen_category ~ ., scales = "free") +
     # scale_color_manual(values = rev(RColorBrewer::brewer.pal(4, "OrRd"))) +
@@ -120,7 +161,8 @@ make_fig_coefficient_avarages <- function(coefficient_averages, variable_importa
           strip.placement = "outside", 
           panel.spacing.y = unit(0, "pt"),
           # panel.grid.major.y = element_line(size = 0.25, colour = "grey70"),
-          axis.text.y = element_text(size = 7, vjust = 0, colour = "black"),
+          # axis.text.y = element_text(size = 7, vjust = 0, colour = "black"),
+          axis.text.y = element_blank(),
           axis.title.y = element_blank(),
           # axis.title.x = element_text(size = 8, colour = "grey20"),
           strip.text = element_text(size = 7, face = "plain", hjust = 0)) +
