ec157d355b5c87ef1b235c7d129a2d8a2b453c59
Fernando Cagua <efcaguab@gmail.com>
2019-02-23 21:06:54 +1300

    Stop using packrat

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 # Prepare workspace -------------------------------------------------------
 
-if(!packrat:::isPackratModeOn()) packrat::on()
+# if(!packrat:::isPackratModeOn()) packrat::on()
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
 library(magrittr)
