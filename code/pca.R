3b82e4745946969df65a0a77f03d5b7239d628d6
Fernando Cagua <efcaguab@gmail.com>
2019-02-26 10:21:57 +1300

    initial commit of code to evaluate reduced competition, Issue #58 (also remove some trailing whitespace along the file)

--- code/pca.R
+++ code/pca.R
@@ -1,16 +1,16 @@
 # put data together to calculate PCA
 get_pca_data <- function(plant_rel_abu, pollen_contribution, degree, org_frame, sites){
-  
+
   pca_data <- plant_rel_abu %>%
     dplyr::full_join(pollen_contribution, by = c("plant_name", "site_name", "var_trans", "scale")) %>%
     dplyr::full_join(degree, by = c("plant_name", "site_name", "var_trans", "scale")) %>%
     dplyr::full_join(org_frame, by = c("plant_name", "site_name", "var_trans", "scale")) %>%
     dplyr::inner_join(sites, by = "site_name") %>%
     dplyr::filter(scale == "community",
-                  var_trans == "log") %>% 
-    dplyr::select(site_name, plant_name, abu, pollen_cont, kn, originality)  %>% 
+                  var_trans == "log") %>%
+    dplyr::select(site_name, plant_name, abu, pollen_cont, kn, originality)  %>%
     dplyr::mutate_at(dplyr::vars(abu, kn), function(x) log(x))
-  
+
   # scale variables across the whole study
   # this gives indication of the flexibility of a plant
   across <- pca_data %>%
@@ -18,7 +18,7 @@ get_pca_data <- function(plant_rel_abu, pollen_contribution, degree, org_frame, 
     dplyr::mutate_if(is.numeric, dplyr::funs(scale)) %>%
     dplyr::mutate_if(is.numeric, dplyr::funs(as.numeric)) %>%
     dplyr::mutate(pca_type = "across")
-  
+
   # scale variables within each community
   # this gives indication of the relative separation within a community
   within <- pca_data %>%
@@ -26,18 +26,18 @@ get_pca_data <- function(plant_rel_abu, pollen_contribution, degree, org_frame, 
     dplyr::mutate_if(is.numeric, dplyr::funs(scale)) %>%
     dplyr::group_by() %>%
     dplyr::mutate(pca_type = "within")
-  
-  dplyr::bind_rows(across, within) 
+
+  dplyr::bind_rows(across, within)
 }
 
 # calculate several flavors of PCA
 get_pca <- function(pca_data, imputation_variants = 0:2){
   pca_data %>%
     split(.$pca_type) %>%
-    purrr::cross2(imputation_variants) %>% 
+    purrr::cross2(imputation_variants) %>%
     purrr::map(~ impute_na_values_pca(.[[1]], .[[2]])) %>%
     purrr::map(get_factominer_pca, scale.unit = F, graph = FALSE)
-}  
+}
 
 # impute variables for a PCA using rows that have at most `na_threshold` NA values
 impute_na_values_pca <- function(x, na_threshold = 0){
@@ -45,18 +45,18 @@ impute_na_values_pca <- function(x, na_threshold = 0){
     dplyr::mutate(n_nas = apply(., 1, function(x) sum(is.na(x)))) %>%
     dplyr::filter(n_nas <= na_threshold) %>%
     dplyr::select(-n_nas)
-  
-  data_numeric <- data_filtered %>% 
-    `class<-`("data.frame") %>% 
+
+  data_numeric <- data_filtered %>%
+    `class<-`("data.frame") %>%
     dplyr::select_if(is.numeric)
-  
+
   if (na_threshold != 0) {
     ncp <- data_numeric %>%
       as.matrix() %>%
       missMDA::estim_ncpPCA() %>%
       extract2("ncp")
-    
-    imputed_data <- data_numeric %>% 
+
+    imputed_data <- data_numeric %>%
       `class<-`("data.frame") %>%
       dplyr::select_if(is.numeric) %>%
       missMDA::imputePCA(ncp = ncp, nb.init = 1) %>%
@@ -75,12 +75,12 @@ impute_na_values_pca <- function(x, na_threshold = 0){
 # wrapper arround the factominer PCA function
 get_factominer_pca <- function(x, ...) {
   # index of columns with qualitative information
-  quali.sup <- lapply(x, is.character) %>% 
+  quali.sup <- lapply(x, is.character) %>%
     unlist() %>%
     which()
-  
-  x %>% 
-    `class<-`("data.frame") %>% 
+
+  x %>%
+    `class<-`("data.frame") %>%
     # dplyr::select_if(is.numeric) %>%
     FactoMineR::PCA(quali.sup = quali.sup, ...)
 }
@@ -106,7 +106,7 @@ distance_between_points <- function(this_pca, col){
     split(.[col]) %>%
     purrr::map(dplyr::select_if, is.numeric) %>%
     purrr::map(dist) %>%
-    purrr::map_dfr(~ data.frame(median_dist = median(., na.rm = TRUE), 
+    purrr::map_dfr(~ data.frame(median_dist = median(., na.rm = TRUE),
                                 mean_dist = mean(., na.rm = TRUE)), .id = col)
 }
 
@@ -120,13 +120,13 @@ get_pc_coords <- function(this_pca){
 # get both distances and volumes from one call and return results in a data frame
 # dims specifies the number of dimensions to be used for the convex hull
 get_pca_sizes_df <- function(this_pca, col, dims = 2){
-  distance <- distance_between_points(this_pca, col) 
+  distance <- distance_between_points(this_pca, col)
   volume <- convex_hull_volume(this_pca, col, dims)
   dplyr::full_join(distance, volume, by = col)
 }
 
 # randomise groupings in a FactoMiner::PCA object
-# randomises the column `col` in the call 
+# randomises the column `col` in the call
 # randomisation can be constrained by `constrain_col`
 randomise_pca_groupings <- function(this_pca, col, constrain_col = NULL){
   random_pca <- this_pca
@@ -157,13 +157,13 @@ run_randomisations_pca <- function(this_pca, col, constrain_col, dims = 2, n_ran
   random <- 1:n_rand %>%
     purrr::map(~ randomise_pca_groupings(this_pca, col, constrain_col)) %>%
     purrr::map_df(get_pca_sizes_df, col, .id = "n_sample")
-    
+
   empirical <- get_pca_sizes_df(this_pca, col, dims = dims) %>%
     dplyr::mutate(n_sample = as.character(0))
-  
-  list(empirical = empirical, 
-       random = random, 
-       pca_type = as.character(this_pca$call$X$pca_type[1]), 
+
+  list(empirical = empirical,
+       random = random,
+       pca_type = as.character(this_pca$call$X$pca_type[1]),
        na_threshold = as.character(this_pca$call$X$threshold[1]))
 }
 
@@ -171,9 +171,9 @@ run_randomisations_pca <- function(this_pca, col, constrain_col, dims = 2, n_ran
 all_randomisations_plant_name <- function(pcas, n_rand){
   pcas %>%
     purrr::keep(function(x) as.character(x$call$X$pca_type[1]) == "across") %>%
-    purrr::map(run_randomisations_pca, 
-               col = "plant_name", 
-               constrain_col = NULL, 
+    purrr::map(run_randomisations_pca,
+               col = "plant_name",
+               constrain_col = NULL,
                dim = 2,
                n_rand = n_rand) %>%
     purrr::map_df(randomisation_as_df)
@@ -183,10 +183,10 @@ all_randomisations_plant_name <- function(pcas, n_rand){
 all_randomisations_site_name <- function(pcas, n_rand){
   pcas %>%
     purrr::keep(function(x) as.character(x$call$X$pca_type[1]) == "within") %>%
-    purrr::map(run_randomisations_pca, 
-               col = "site_name", 
-               constrain_col = "plant_name", 
-               dim = 2, 
+    purrr::map(run_randomisations_pca,
+               col = "site_name",
+               constrain_col = "plant_name",
+               dim = 2,
                n_rand = n_rand) %>%
     purrr::map_df(randomisation_as_df)
 }
@@ -212,69 +212,69 @@ get_permanova <- function(x, col){
 
 plot_permanova_dist <- function(permanova_plant_distances, permanova_site_distances){
   require(ggplot2)
-  
+
   perma_data <- permanova_plant_distances %>%
     dplyr::filter(na_threshold %in% c(0,1)) %>%
     dplyr::group_by(plant_name) %>%
     dplyr::mutate(mean_value = mean(value, na.rm = T)) %>%
-    dplyr::group_by() %>% 
-    dplyr::filter(!is.na(mean_value), 
-                  metric == "median_dist", 
-                  na_threshold == 0, 
-                  !is.na(value)) %>% 
+    dplyr::group_by() %>%
+    dplyr::filter(!is.na(mean_value),
+                  metric == "median_dist",
+                  na_threshold == 0,
+                  !is.na(value)) %>%
     # dplyr::mutate(plant_name = paste0("  ", plant_name, "  ")) %>%
     dplyr::mutate(plant_name = shorten_sp_name(plant_name)) %>%
-    dplyr::mutate(plant_name = forcats::fct_reorder(plant_name, value, .desc = F), 
+    dplyr::mutate(plant_name = forcats::fct_reorder(plant_name, value, .desc = F),
                   value_adj = p.adjust(value, method = "BH"))
-  
+
   nudge <- 0
-  perma_data %>% 
-    dplyr::mutate(x_label = dplyr::if_else(value < 0.05, 
+  perma_data %>%
+    dplyr::mutate(x_label = dplyr::if_else(value < 0.05,
                                            max(value) + nudge, min(value) - nudge)) %>%
     ggplot(aes(x = value, y = plant_name)) +
-    geom_tile(aes(width = Inf, height = 1, 
-                  alpha = as.numeric(plant_name) %% 2 == 0), 
+    geom_tile(aes(width = Inf, height = 1,
+                  alpha = as.numeric(plant_name) %% 2 == 0),
               fill = cgm()$fill_rows) +
-    geom_vline(xintercept = 0.05, linetype = 2, 
-               size = cgm()$size_references, 
+    geom_vline(xintercept = 0.05, linetype = 2,
+               size = cgm()$size_references,
                color = cgm()$color_references) +
-    geom_segment(aes(xend = 0.05, yend = plant_name), size = 0.25, 
+    geom_segment(aes(xend = 0.05, yend = plant_name), size = 0.25,
                  color = "grey50") +
-    geom_point(shape = 21, 
-               size = 1, 
-               fill = cgm()$pal_rb3[2], 
+    geom_point(shape = 21,
+               size = 1,
+               fill = cgm()$pal_rb3[2],
                color = cgm()$color_errorbars) +
-    geom_text(aes(label = plant_name, x = x_label), 
-              fontface = "italic", 
-              size = 2, 
+    geom_text(aes(label = plant_name, x = x_label),
+              fontface = "italic",
+              size = 2,
               hjust = "inward") +
     geom_point(aes(y = 13.5, x = 0.05), alpha = 0) +
-    annotate(geom = "text", x = 0.05, y = 13, 
-             label = expression(" " %->% plain("more flexible") %->% ""), 
-             parse = F, 
-             hjust = "left", 
-             size = 2.25, 
+    annotate(geom = "text", x = 0.05, y = 13,
+             label = expression(" " %->% plain("more flexible") %->% ""),
+             parse = F,
+             hjust = "left",
+             size = 2.25,
              colour = "grey20") +
     geom_point(aes(y = -0.5, x = 0.05), alpha = 0) +
-    annotate(geom = "text", x = 0.05, y = 0, 
-             label = expression(" " %<-% plain("significantly smaller") %<-% ""), 
-             parse = F, 
-             hjust = "right", 
-             size = 2.25, 
+    annotate(geom = "text", x = 0.05, y = 0,
+             label = expression(" " %<-% plain("significantly smaller") %<-% ""),
+             parse = F,
+             hjust = "right",
+             size = 2.25,
              colour = "grey20") +
     pub_theme() +
-    theme(legend.position = "none", 
-          axis.title.y = element_blank(), 
-          axis.text.y = element_blank(), 
-          axis.ticks.y = element_blank(), 
-          panel.border = element_blank(), 
+    theme(legend.position = "none",
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          panel.border = element_blank(),
           axis.line.x = element_line(size = 0.25)) +
-    labs(x = expression(italic(p) - plain("value")), 
-         title = "(c) flexibility of plant's strategies", 
+    labs(x = expression(italic(p) - plain("value")),
+         title = "(c) flexibility of plant's strategies",
          subtitle = "median distance between plant strategies vs. randomisations") +
     coord_cartesian(clip = "off") +
-    scale_x_continuous(expand = c(0,0.1), 
-                       breaks = c(0.01, 0.05, 0.5, 0.99), 
+    scale_x_continuous(expand = c(0,0.1),
+                       breaks = c(0.01, 0.05, 0.5, 0.99),
                        trans = "log") +
     scale_y_discrete(expand = c(0,0)) +
     scale_alpha_manual(values = c(0,1))
@@ -282,41 +282,41 @@ plot_permanova_dist <- function(permanova_plant_distances, permanova_site_distan
 }
 
 plot_pca <- function(pcas, chosen_threshold){
-  
+
   require(ggplot2)
 
   this_pca <- pcas %>%
     purrr::keep(~ .$call$X$pca_type[1] == "across") %>%
     purrr::keep(~ .$call$X$threshold[1] == chosen_threshold) %>%
     extract2(1)
-  
+
   this_pca_data <- this_pca$call$X %>%
     dplyr::bind_cols(tibble::as_data_frame(this_pca$ind$coord)) %>%
     dplyr::group_by(plant_name) %>%
     dplyr::mutate(n_sites = dplyr::n_distinct(site_name)) %>%
     # dplyr::group_by() %>%
     dplyr::filter(n_sites > 1)
-  
+
   variances_data <- this_pca$eig %>%
-    as.data.frame() %>% 
+    as.data.frame() %>%
     tibble::rownames_to_column() %>%
     dplyr::mutate(rowname = stringr::str_remove(rowname, "comp ")) %>%
-    dplyr::rename(dim = rowname) 
-  
+    dplyr::rename(dim = rowname)
+
   hulls <- this_pca_data %>%
     dplyr::group_by(plant_name) %>%
-    dplyr::select_at(dplyr::vars(dplyr::contains("Dim"))) %>% 
+    dplyr::select_at(dplyr::vars(dplyr::contains("Dim"))) %>%
     dplyr::do(.[grDevices::chull(.[, -1L]), ]) %>%
     dplyr::filter(plant_name %in% unique(this_pca_data$plant_name))
-  
+
   this_pca_data %>%
     ggplot(aes(x = Dim.1, y = Dim.2)) +
-    geom_hline(yintercept = 0, 
-               linetype = 2, 
+    geom_hline(yintercept = 0,
+               linetype = 2,
                colour = cgm()$color_references,
                size = cgm()$size_references) +
-    geom_vline(xintercept = 0, 
-               linetype = 2, 
+    geom_vline(xintercept = 0,
+               linetype = 2,
                colour = cgm()$color_references,
                size = cgm()$size_references) +
     geom_polygon(data = hulls,
@@ -326,21 +326,21 @@ plot_pca <- function(pcas, chosen_threshold){
                  size = cgm()$size_errorbars,
                  alpha = 0.25) +
     geom_point(data = hulls,
-      # aes( fill = plant_name), 
-               shape = 21, 
+      # aes( fill = plant_name),
+               shape = 21,
                fill = cgm()$pal_rb3[2],
-               colour = cgm()$color_errorbars, 
+               colour = cgm()$color_errorbars,
                size = 1) +
     pub_theme() +
     scale_color_viridis_d() +
     scale_fill_viridis_d() +
     # theme(legend.position = "none") +
     # coord_equal() +
-    labs(x = paste0("1st component (", round(variances_data$`percentage of variance`[1]), "%)"), 
-         y = paste0("1nd component (", round(variances_data$`percentage of variance`[2]), "%)"), 
-         title = "(b) plant strategies in PCA space", 
+    labs(x = paste0("1st component (", round(variances_data$`percentage of variance`[1]), "%)"),
+         y = paste0("1nd component (", round(variances_data$`percentage of variance`[2]), "%)"),
+         title = "(b) plant strategies in PCA space",
          subtitle = "convex hulls of species in two communities or more")
-  
+
 }
 
 plot_pca_variances_and_contributions <- function(pcas, chosen_threshold){
@@ -350,64 +350,77 @@ plot_pca_variances_and_contributions <- function(pcas, chosen_threshold){
     purrr::keep(~ .$call$X$pca_type[1] == "across") %>%
     purrr::keep(~ .$call$X$threshold[1] == chosen_threshold) %>%
     extract2(1)
-  
+
   variances_data <- this_pca$eig %>%
-    as.data.frame() %>% 
+    as.data.frame() %>%
     tibble::rownames_to_column() %>%
     dplyr::mutate(rowname = stringr::str_remove(rowname, "comp ")) %>%
     dplyr::rename(dim = rowname) %>%
     dplyr::mutate_if(is.numeric, function(x) x/100) %>%
     dplyr::mutate(group = "components' cumulative percentage of variance")
-  
+
   contributions_data <- this_pca$var$contrib %>%
-    as.data.frame() %>% 
+    as.data.frame() %>%
     tibble::rownames_to_column() %>%
     tidyr::gather(key = "dim", "contribution", dplyr::contains("Dim")) %>%
-    dplyr::mutate(dim = stringr::str_remove(dim, "Dim."), 
+    dplyr::mutate(dim = stringr::str_remove(dim, "Dim."),
                   contribution = contribution/100) %>%
     dplyr::rename(term = rowname)
-  
+
   contributions_data %>%
-    humanize(term_long = F) %>% 
+    humanize(term_long = F) %>%
     dplyr::mutate(term = forcats::fct_relevel(term, "visit effectiv.", "abundance", "func. originality")) %>%
     ggplot(aes(x = as.numeric(dim), y = contribution)) +
-    geom_col(aes(fill = term), alpha = 0.75, 
+    geom_col(aes(fill = term), alpha = 0.75,
              position = position_stack(reverse = TRUE)) +
-    geom_line(data = variances_data, 
-              aes(y = `cumulative percentage of variance`, 
-                  linetype = group), 
-              size = 0.5, 
+    geom_line(data = variances_data,
+              aes(y = `cumulative percentage of variance`,
+                  linetype = group),
+              size = 0.5,
               colour = cgm()$color_errorbars) +
     geom_point(data = variances_data,
-               aes(y = `cumulative percentage of variance`, 
-                   shape = group), 
-               size = 1, 
-               # shape = 21, 
-               fill = cgm()$pal_rb3[2], 
+               aes(y = `cumulative percentage of variance`,
+                   shape = group),
+               size = 1,
+               # shape = 21,
+               fill = cgm()$pal_rb3[2],
                colour = cgm()$color_errorbars) +
     scale_x_continuous(breaks =1:4, trans = "reverse", labels = scales::ordinal(1:4)) +
     scale_y_continuous(labels = scales::percent, expand = c(0,0)) +
     scale_fill_brewer(palette = "Greys", direction = -1) +
     scale_shape_manual(values = 21) +
     scale_linetype_manual(values = 1) +
-    guides(fill = guide_legend(order = 2, title = ""), 
-           shape = guide_legend(order = 1, title = ""), 
+    guides(fill = guide_legend(order = 2, title = ""),
+           shape = guide_legend(order = 1, title = ""),
            linetype = guide_legend(order = 1, title = "")) +
     pub_theme() +
     coord_flip(clip = "off") +
-    labs(x = "component", 
-         title = "(a) components' variance and variable contributions", 
+    labs(x = "component",
+         title = "(a) components' variance and variable contributions",
          subtitle = "principal component analysis of ecological variables") +
-    theme(panel.border = element_blank(), 
-          axis.line.x = element_line(size = 0.25), 
-          legend.position = "top", 
-          axis.ticks.y = element_blank(), 
-          legend.box = "vertical", 
-          legend.spacing = unit(1, "pt"), 
-          plot.margin = margin(t = 5.5, r = 8, b = 5.5, l = 5.5, unit = "pt"), 
-          legend.text = element_text(margin = margin(r = 0, l = -1, unit = "pt")), 
-          legend.box.just = "right", 
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size = 0.25),
+          legend.position = "top",
+          axis.ticks.y = element_blank(),
+          legend.box = "vertical",
+          legend.spacing = unit(1, "pt"),
+          plot.margin = margin(t = 5.5, r = 8, b = 5.5, l = 5.5, unit = "pt"),
+          legend.text = element_text(margin = margin(r = 0, l = -1, unit = "pt")),
+          legend.box.just = "right",
           legend.margin = margin(),
           legend.box.margin = margin(b = -10))
-  
+
+}
+
+# Evaluate competition of alternative strategies
+tinkering <- function(glanced_fixed, tidied_fixed, include_random = FALSE) {
+  loadd(glanced_fixed)
+  loadd(tidied_fixed)
+
+  a <- dplyr::inner_join(glanced_fixed, tidied_fixed) %>%
+    # Using the regex code \.\b so that in cases like Plant sp..MA_SC_R_1 the
+    # string gets splited in the dot that is next to the second word boundary
+    tidyr::separate(col = level, into = c("plant_name", "site_name"), sep = "\\.\\b") %>%
+    dplyr::group_by(model, pollen_category, scale, var_trans) %>%
+    dplyr::distinct(model, pollen_category, scale, var_trans, fixed_formula, estimate, AIC)
 }
