66299e9f9ca8095c8ba06234759333e39b902c1d
Fernando Cagua <efcaguab@gmail.com>
2019-03-11 12:17:02 +1300

    Y labels of figure 2 are now always centered. Close #52

--- code/coefficient_averages.R
+++ code/coefficient_averages.R
@@ -1,29 +1,29 @@
 get_coefficient_averages <- function(tidied_fixed, model_formula_ranking, N = 99){
   model_weights <- model_formula_ranking$by_model_set %>%
     dplyr::group_by(pollen_category, scale) %>%
-    dplyr::mutate(l = get_likelyhoods(delta_AIC_median), 
-                  w = get_weights(l)) %>% 
+    dplyr::mutate(l = get_likelyhoods(delta_AIC_median),
+                  w = get_weights(l)) %>%
     dplyr::select(pollen_category, scale, fixed_formula, w)
-    
+
   1:N %>%
     purrr::map_df(get_coeficient_estimate_sample, tidied_fixed, model_weights, N)
-  
+
 }
 
 get_coeficient_estimate_sample <- function(sample_n, tidied_fixed, model_weights, N){
   # consider cases with zero
-  
+
   tidied_fixed %>%
-    dplyr::filter(term != "(Intercept)") %>% 
+    dplyr::filter(term != "(Intercept)") %>%
     dplyr::group_by(model, pollen_category, scale, fixed_formula, term) %>%
-    dplyr::summarise(estimate = mean(estimate)) %>% 
-    dplyr::group_by() %>% 
+    dplyr::summarise(estimate = mean(estimate)) %>%
+    dplyr::group_by() %>%
     # complete cases. The estimate of the term for the models in which it wasn't present is zero
-    tidyr::complete(model, pollen_category, scale, fixed_formula, term, fill = list(estimate = 0)) %>% 
-    dplyr::inner_join(model_weights, 
-                      by = c("pollen_category", "scale", "fixed_formula")) %>% 
+    tidyr::complete(model, pollen_category, scale, fixed_formula, term, fill = list(estimate = 0)) %>%
+    dplyr::inner_join(model_weights,
+                      by = c("pollen_category", "scale", "fixed_formula")) %>%
     dplyr::group_by(pollen_category, scale, term) %>%
-    dplyr::sample_n(size = N, replace = T, weight = w) %>% 
+    dplyr::sample_n(size = N, replace = T, weight = w) %>%
     # dplyr::summarise(estimate = weighted.mean(estimate, w)) %>%
     dplyr::mutate(sample_n = sample_n)
 }
@@ -45,24 +45,24 @@ plot_coefficient_averages <- function(coefficient_averages, variable_importance)
       tidyr::gather("key", "value", `conspecific (absolute)`:heterospecific) %>%
       dplyr::filter(key %in% c("conspecific (absolute)", "heterospecific")) %>%
       dplyr::mutate(term = forcats::fct_reorder(term, value))
-    
+
     plot_metric_qual_quan <- function(x, this_metric){
       # annotations <- tibble::data_frame(
-      #   x = 5, 
-      #   y = 0, 
-      #   label = c("\u2665 positive effect →", "negative effect"), 
-      #   hjust = c(0, 1), 
+      #   x = 5,
+      #   y = 0,
+      #   label = c("\u2665 positive effect →", "negative effect"),
+      #   hjust = c(0, 1),
       #   metric = NA
       # )
-      
+
       abs_smaller <- function(x,y) {if (abs(x) < abs(y)) return(x) ; y}
       abs_larger <-  function(x,y) {if (abs(x) < abs(y)) return(y) ; x}
-      
+
       line_offset <- 0.25
       p <- x %>%
         dplyr::filter(metric == this_metric) %>%
         dplyr::rowwise() %>%
-        dplyr::mutate(label_y = dplyr::if_else(abs(estimate_mid) < 0.25, 
+        dplyr::mutate(label_y = dplyr::if_else(abs(estimate_mid) < 0.25,
                                                abs_larger(mean_estimate_quantile_025, mean_estimate_quantile_975),
                                                abs_smaller(mean_estimate_quantile_025, mean_estimate_quantile_975)),
                       label_hjust = dplyr::if_else(abs(estimate_mid) > 0.25,
@@ -70,7 +70,7 @@ plot_coefficient_averages <- function(coefficient_averages, variable_importance)
                       "outward")) %>%
         # dplyr::filter(metric == this_metric) %>%
         ggplot(aes(y = estimate_mid, x = term, group = metric)) +
-        geom_tile(aes(height = Inf, width = 1, alpha = as.numeric(term) %% 2 == 0), 
+        geom_tile(aes(height = Inf, width = 1, alpha = as.numeric(term) %% 2 == 0),
                   fill = cgm()$fill_rows) +
         geom_hline(yintercept = 0, linetype = 2, size = 0.25, colour = "gray30") +
         geom_text(aes(#label = "..",
@@ -79,73 +79,73 @@ plot_coefficient_averages <- function(coefficient_averages, variable_importance)
                       hjust = label_hjust,
                       alpha = metric == this_metric),
                   size = 2,
-                  # nudge_x = line_offset, 
+                  # nudge_x = line_offset,
                   vjust = 0.5) +
-        # geom_errorbar(aes(x = as.numeric(term) - line_offset, ymin = estimate_quantile_025, 
-        #                   ymax = estimate_quantile_975, 
-        #                   alpha = metric == this_metric), 
-        #               colour = cgm()$color_errorbars, 
-        #               size = cgm()$size_errorbars, 
+        # geom_errorbar(aes(x = as.numeric(term) - line_offset, ymin = estimate_quantile_025,
+        #                   ymax = estimate_quantile_975,
+        #                   alpha = metric == this_metric),
+        #               colour = cgm()$color_errorbars,
+        #               size = cgm()$size_errorbars,
         #               width = 0,
         #               position = position_dodge(width = dodge_width)) +
-        geom_errorbar(aes(x = term, ymin = mean_estimate_quantile_025, 
-                          ymax = mean_estimate_quantile_975, 
-                          alpha = metric == this_metric), 
-                      colour = cgm()$color_errorbars, 
-                      size = cgm()$size_errorbars, 
+        geom_errorbar(aes(x = term, ymin = mean_estimate_quantile_025,
+                          ymax = mean_estimate_quantile_975,
+                          alpha = metric == this_metric),
+                      colour = cgm()$color_errorbars,
+                      size = cgm()$size_errorbars,
                       width = 0,
                       position = position_dodge(width = dodge_width)) +
         geom_point(aes(x = term, alpha = metric == this_metric),
                    colour = cgm()$color_errorbars, shape = 21,
                    fill = cgm()$pal_rb3[2],
-                   size = 1, 
+                   size = 1,
                    position = position_dodge(width = dodge_width)) +
-        # geom_text(data = annotations, aes(x = x, y = y, label = label, hjust = hjust), 
+        # geom_text(data = annotations, aes(x = x, y = y, label = label, hjust = hjust),
                   # size = 2) +
         pub_theme() +
         labs(y = "effect size") +
         scale_x_discrete(expand = c(0,0)) +
         coord_flip(clip = "off") +
-        theme(legend.position = "none", 
-              axis.title.y = element_blank(), 
+        theme(legend.position = "none",
+              axis.title = element_blank(),
               axis.ticks.y = element_blank(),
-              axis.text.y = element_text(hjust = 0.5), 
-              panel.border = element_blank(), 
+              axis.text.y = element_blank(),
+              panel.border = element_blank(),
               axis.line.x = element_line(size = 0.25)) +
         scale_alpha_manual(values = c(0,1))
-      
+
       if (this_metric == "quality") {
         p <- p + scale_y_continuous(limits = c(-0.24, 0.35))
       }
       p
     }
-    
+
     dodge_width <- 0
     qua_qua_data <- coefficient_averages  %>%
       dplyr::group_by(scale, pollen_category, term, sample_n) %>%
-      dplyr::mutate(sample_n2 = 1:n()) %>% 
+      dplyr::mutate(sample_n2 = 1:n()) %>%
       dplyr::select(pollen_category, term, sample_n, sample_n2, estimate) %>%
-      tidyr::spread(pollen_category, estimate) %>% 
+      tidyr::spread(pollen_category, estimate) %>%
       dplyr::mutate(quantity_abs = conspecific_abs,
                     quality_abs = conspecific_abs - heterospecific_abs) %>%
       dplyr::select(scale, term, sample_n, dplyr::contains("qua")) %>%
-      tidyr::gather("var", "estimate", dplyr::contains("qua")) %>% 
+      tidyr::gather("var", "estimate", dplyr::contains("qua")) %>%
       dplyr::group_by(scale, term, sample_n, var) %>%
       dplyr::mutate(mean_estimate = mean(estimate)) %>%
       dplyr::group_by(scale, term, var) %>%
       # dplyr::sample_n(9, replace = TRUE) %>%
-      dplyr::summarise_at(c("estimate", "mean_estimate"), dplyr::funs(mid, lower, upper, quantile_025, quantile_975)) %>% 
+      dplyr::summarise_at(c("estimate", "mean_estimate"), dplyr::funs(mid, lower, upper, quantile_025, quantile_975)) %>%
       tidyr::separate(col = var, into = c("metric", "type"), sep = "_", remove = FALSE) %>%
-      dplyr::mutate(type = paste0(type, ".")) %>% 
+      dplyr::mutate(type = paste0(type, ".")) %>%
       dplyr::group_by() %>%
       humanize() %>%
       dplyr::mutate(term = factor(term, levels(var_imp$term)))
-   
-    plots <- purrr::map(c("quantity", "quality"), 
+
+    plots <- purrr::map(c("quantity", "quality"),
                         ~ plot_metric_qual_quan(qua_qua_data, .)) %>%
-      purrr::map2(c("quantity (conspecific)", "quality (consp. / heterosp.)"), 
-                  function(x,y) {x + labs(subtitle = y)}) 
-    
+      purrr::map2(c("quantity (conspecific)", "quality (consp. / heterosp.)"),
+                  function(x,y) {x + labs(subtitle = y)})
+
     plots[[1]] <- plots[[1]] +
       theme(axis.text.y = element_blank(),
             plot.margin = margin(t = 5.5, r = 0, b = 5.5, l = 5.5, unit = "pt"),
@@ -153,16 +153,28 @@ plot_coefficient_averages <- function(coefficient_averages, variable_importance)
             axis.line.x = element_line(size = 0.25)) +
       labs(title = "(c) mean effect on pollination service")
     plots[[2]] <- plots[[2]] + theme(plot.subtitle = element_text(hjust = 1),
-                                     axis.line.x = element_line(size = 0.25), 
-                                     axis.text.y = element_text(size = 6.5, colour = "black"))
-      
-  #   pdf(width = 3.25 * 4/3, height = 1.5)
+                                     axis.line.x = element_line(size = 0.25),
+                                     plot.margin = margin(t = 5.5, r = 5.5, b = 5.5, l = 0, unit = "pt"))
+
+    plots[[3]] <- qua_qua_data %>%
+      ggplot(aes(y = term)) +
+      geom_text(aes(label = term), x = 0.5, stat = "unique", size = 2.3)+
+      # expansion to match the 90% width of the columns
+      scale_y_discrete(expand = c(0,0.5)) +
+      theme_minimal() +
+      theme(axis.text = element_blank(),
+            axis.title = element_blank(),
+            panel.grid = element_blank(),
+            plot.margin = margin(r = 0, t = 5.5, b = 5.5, l = 0))
+  #
+  #   pdf(width = 3.25, height = 1.5)
   #   cowplot::plot_grid(
   #     plots[[1]],
+  #     plots[[3]],
   #     plots[[2]],
-  #     ncol = 2,
+  #     ncol = 3,
   #     align = "h",
-  #     rel_widths = c(1, 1.5) # for two thirds figure its 1:1.4
+  #     rel_widths = c(1, 0.75, 1) # for two thirds figure its 1:1.4
   #   )
   # dev.off()
   plots
--- code/get_variable_importance.R
+++ code/get_variable_importance.R
@@ -1,98 +1,112 @@
 get_variable_importance <- function(model_formula_ranking){
-  
+
   # get the terms in the models
   terms <- model_formula_ranking$aggregated$fixed_formula %>%
     stringr::str_remove("pollen_gain ~  ") %>%
     stringr::str_split(stringr::fixed(" + ")) %>%
     unlist() %>%
-    unique() 
+    unique()
   # remove the null model
   terms <- terms[terms != "1"]
-  
+
   model_weights <- model_formula_ranking$by_model_set %>%
     dplyr::filter(scale == "community") %>%
     dplyr::group_by(pollen_category) %>%
-    dplyr::mutate(likelyhood = get_likelyhoods(delta_AIC_median), 
+    dplyr::mutate(likelyhood = get_likelyhoods(delta_AIC_median),
                   weight = get_weights(likelyhood))
-  
+
   crosses <- purrr::cross2(terms, unique(model_formula_ranking$by_model_set$pollen_category))
   crosses %>%
     purrr::map(~model_weights$weight[stringr::str_detect(model_weights$fixed_formula, .[[1]]) & model_weights$pollen_category == .[[2]]]) %>%
     purrr::map(sum) %>%
-    purrr::map2_df(crosses, 
+    purrr::map2_df(crosses,
                    ~ dplyr::data_frame(
                      var = .y[[1]],
-                     pollen_category = .y[[2]], 
+                     pollen_category = .y[[2]],
                      importance = .x)
     ) %>%
     dplyr::mutate(importance = round(importance, digits = 3)) %>%
     dplyr::rename(term = var) %>%
-    humanize() %>% 
+    humanize() %>%
     tidyr::spread(pollen_category, importance) %>%
     dplyr::arrange(dplyr::desc(heterospecific))
 }
 
 plot_variable_importance <- function(variable_importance) {
   require(ggplot2)
-  
+
   plot_importance <- function(x){
     middle_space <- -0.4  # for 1/3 is -0.525
     x %>% ggplot(aes(x = term, y = value)) +
       # geom_segment(aes(xend = term, yend = 0)) +
       # geom_point(shape = 21, fill = cgm()$pal_rb3[2], size = 6) +
-      
+
       geom_col(fill = "grey90") +
       # geom_hline(yintercept = 0, linetype = 2, size = 0.25, colour = "grey30") +
-      
-      geom_point(aes(x = 1, y = middle_space), alpha = 0) +
-      geom_text(aes(label = paste0(" ", format(round(value, digits = 2)), " "), 
-                    hjust = text_align), 
+
+      # geom_point(aes(x = 1, y = middle_space), alpha = 0) +
+      geom_text(aes(label = paste0(" ", format(round(value, digits = 2)), " "),
+                    hjust = text_align),
                 size = 2) +
-      geom_text(aes(label = term, y = middle_space), 
-                # fontface = "bold",
-                size = 2.5, 
-                colour = "black") +
+      # geom_text(aes(label = term, y = middle_space),
+      #           # fontface = "bold",
+      #           size = 2.5,
+      #           colour = "black") +
       scale_x_discrete(expand = c(0,0)) +
       coord_flip() +
       pub_theme() +
-      theme(axis.title.y = element_blank(), 
-            legend.position = "none", 
+      theme(axis.title.y = element_blank(),
+            legend.position = "none",
             panel.border = element_blank(),
             axis.ticks = element_blank(),
-            axis.title.x = element_blank(), 
+            axis.title = element_blank(),
             axis.text = element_blank())
   }
-  
+
   plots <- variable_importance %>%
     tidyr::gather("key", "value", `conspecific (absolute)`:heterospecific) %>%
     dplyr::filter(key %in% c("conspecific (absolute)", "heterospecific")) %>%
-    dplyr::mutate(term = forcats::fct_reorder(term, value), 
-                  # text_align = dplyr::if_else(value < 0.1, 
+    dplyr::mutate(term = forcats::fct_reorder(term, value),
+                  # text_align = dplyr::if_else(value < 0.1,
                                               # "outward", "inward")) %>%
-                  text_align = dplyr::if_else(key == "heterospecific", 
+                  text_align = dplyr::if_else(key == "heterospecific",
                                               "right", "left")) %>%
     split(.$key) %>%
     purrr::map(plot_importance)
-  
+
   plots <- plots %>%
     # subtitles
-    purrr::map2(c("conspecific pollen", "heterospecific pollen"), 
+    purrr::map2(c("conspecific pollen", "heterospecific pollen"),
               function(x,y) {x + labs(subtitle = y)}) %>%
-    purrr::map2(list(margin(r = 0, t = 5.5, b = 5.5, l = 5.5), margin(l = 0, r = 5.5, t = 5.5, b = 5.5)), 
+    purrr::map2(list(margin(r = 0, t = 5.5, b = 5.5, l = 5.5), margin(l = 0, r = 5.5, t = 5.5, b = 5.5)),
                 function(x,y) {x + theme(plot.margin = y)})
-  
+
+  plots[[3]] <- variable_importance %>%
+    ggplot(aes(y = term)) +
+    geom_text(aes(label = term), x = 0.5, size = 2.3) +
+    # expansion to match the 90% width of the columns
+    scale_y_discrete(expand = c(0,0.45)) +
+    theme_minimal() +
+    theme(axis.text = element_blank(),
+          axis.title = element_blank(),
+          panel.grid = element_blank(),
+          plot.margin = margin(r = 0, t = 5.5, b = 5.5, l = 0))
+
   plots[[1]] <- plots[[1]] + labs(title = "(a) relative variable importance")
-  plots[[2]] <- plots[[2]] + scale_x_discrete(position = "top", expand = c(0,0))
+  # plots[[2]] <- plots[[2]] + scale_x_discrete(position = "top", expand = c(0,0))
   plots[[1]] <- plots[[1]] + scale_y_reverse(expand = c(0,0))
   plots[[2]] <- plots[[2]] + scale_y_continuous(expand = c(0,0))
   plots[[2]] <- plots[[2]] + theme(plot.subtitle = element_text(hjust = 1))
   plots[[1]] <- plots[[1]] + theme(plot.title = element_text(margin = margin(b = 5.5, unit = "pt")))
-  
+
   # pdf(width = 3.25 , height = 1.25)
   # cowplot::plot_grid(
-  #   plotlist = plots, 
-  #   ncol = 2, 
-  #   align = "h"
+  #   plots[[1]],
+  #   plots[[3]],
+  #   plots[[2]],
+  #   align = "h",
+  #   rel_widths = c(1,0.75,1),
+  #   nrow = 1
   # )
   # dev.off()
   plots
